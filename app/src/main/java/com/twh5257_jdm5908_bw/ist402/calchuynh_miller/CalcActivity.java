8b8f18e8fa83c7d2f3a956f0ce3a24e541350cae
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-02 01:01:52 -0500

    Initial commit

---
+++ app/src/main/java/com/twh5257_jdm5908_bw/ist402/calchuynh_miller/CalcActivity.java
@@ -0,0 +1,13 @@
+package com.twh5257_jdm5908_bw.ist402.calchuynh_miller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
